acc93bc866679869f3e8307142e8eeb9eb0d0779
Ana Hernandez <ashernadez3@soy.sena.edu.co>
2023-11-08 15:44:58 -0500

    add PostOrder with bad programming practices

--- ApiRestBilling/ApiRestBilling/Controllers/OrdersController.cs
+++ ApiRestBilling/ApiRestBilling/Controllers/OrdersController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiRestBilling.Data;
+using ApiRestBilling.Models;
+using ApiRestBilling.Services;
 
 namespace ApiRestBilling.Controllers
 {
@@ -8,36 +15,127 @@ namespace ApiRestBilling.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
-        // GET: api/<OrdersController>
+        private readonly ApplicationDbContext _context;
+        private readonly IPurchaseOrdersService _purchaseOrdersService;
+
+        public OrdersController(ApplicationDbContext context, IPurchaseOrdersService purchaseOrdersService)
+        {
+            _context = context;
+            _purchaseOrdersService = purchaseOrdersService;
+        }
+
+        // GET: api/Orders
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
         {
-            return new string[] { "value1", "value2" };
+            if (_context.Orders == null)
+            {
+                return NotFound();
+            }
+            return await _context.Orders.Include(oi => oi.OrderItems).ToListAsync();
         }
 
-        // GET api/<OrdersController>/5
+        // GET: api/Orders/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Order>> GetOrder(int id)
         {
-            return "value";
+            if (_context.Orders == null)
+            {
+                return NotFound();
+            }
+            // var order = await _context.Orders.Include(oi => oi.OrderItems).FindAsync(id);
+            var order = await _context.Orders.Include(oi => oi.OrderItems)
+                                     .FirstOrDefaultAsync(o => o.Id == id); // Asumiendo que el nombre del campo es 'Id'.
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
         }
 
-        // POST api/<OrdersController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<OrdersController>/5
+        // PUT: api/Orders/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutOrder(int id, Order order)
         {
+            if (id != order.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<OrdersController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
         {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Orders'  is null.");
+            }
+
+            // Recorremos cada detalle en la orden de compra
+            foreach (var detalle in order.OrderItems)
+            {
+                // Asignar el precio unitario del producto al detalle
+                detalle.UnitPrice = await _purchaseOrdersService.CheckUnitPrice(detalle);
+
+                // Calcular el subtotal
+                detalle.Subtotal = await _purchaseOrdersService.CalculateSubtotalOrderItem(detalle);
+            }
+
+            // Asignar el total calculado a la orden de compra (si tienes una propiedad explicita para el total en tu modelo)
+            order.TotalAmount = _purchaseOrdersService.CalcularTotalOrderItems((List<OrderItem>)order.OrderItems);
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return NotFound();
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderExists(int id)
+        {
+            return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
--- ApiRestBilling/ApiRestBilling/Controllers/ProductsController.cs
+++ ApiRestBilling/ApiRestBilling/Controllers/ProductsController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiRestBilling.Data;
+using ApiRestBilling.Models;
 
 namespace ApiRestBilling.Controllers
 {
@@ -8,36 +14,111 @@ namespace ApiRestBilling.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        // GET: api/<ProductsController>
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return new string[] { "value1", "value2" };
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            return await _context.Products.ToListAsync();
         }
 
-        // GET api/<ProductsController>/5
+        // GET: api/Products/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "value";
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
         }
 
-        // POST api/<ProductsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProductsController>/5
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutProduct(int id, Product product)
         {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // DELETE api/<ProductsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
         {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Products'  is null.");
+            }
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
