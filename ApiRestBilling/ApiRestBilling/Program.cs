32504128cdf684458e4a68d217a04f8e6aba2a19
brayan cardenas <140582245+michmichyh@users.noreply.github.com>
2023-12-11 15:53:34 -0500

    feat(backend): add final

--- ApiRestBilling/ApiRestBilling/Program.cs
+++ ApiRestBilling/ApiRestBilling/Program.cs
@@ -32,6 +32,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
